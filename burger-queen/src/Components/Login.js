ff113f3f338966151f16d8d25ee97c14c206a4bb
mssroxy <roxysolanopino@gmail.com>
2022-05-20 10:04:15 -0400

    guardando data

--- burger-queen/src/Components/Login.js
+++ burger-queen/src/Components/Login.js
@@ -1,29 +1,41 @@
-import React from "react";
-import './Login.css';
-import logo from "./img/logoburger.png"
-
-// export class UserLogin extends React.Component {
-//     render() {
-//         return (
-//         <div className= "container-login"> 
-//         <h1>HOLAAAAAAA</h1>        
-//         </div>
-//         )
-//     }
-// }
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../img/logoburger.png";
 
 export function UserLogin() {
-    return (
-        <div className="div-login">
-            <div className="div-logo">
-                <img src={logo} alt="Logo BQ" className="logo-img"/>
-            </div>
-            <form className="form-login">
-                <h1>Iniciar Sesión</h1>
-                <input className = "email" placeholder="Ingresa tu correo electronico"/>
-                <input className = "password" type = "password" placeholder="Ingresa tu contraseña"/>
-                <button className="btn-login">INGRESAR</button>
-            </form>
-        </div>
-    )
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`${username}, ${password}`)
+  }
+
+  return (
+    <div className="div-login">
+      <div className="div-logo">
+        <img src={logo} alt="Logo BQ" className="logo-img" />
+      </div>
+
+      <form className="form-login" onSubmit={handleSubmit}>
+        <h1>Iniciar Sesión</h1>
+
+        <input
+          className="email"
+          placeholder="Ingresa tu correo electronico"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="password"
+          type="password"
+          placeholder="Ingresa tu contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn-login">INGRESAR</button>
+      </form>
+    </div>
+  );
 }
