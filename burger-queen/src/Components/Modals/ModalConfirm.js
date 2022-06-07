7836fa0f2e103cd74afecda5acd96fe5002b6bdc
mssroxy <roxysolanopino@gmail.com>
2022-06-07 13:20:47 -0400

    modales en construccion

--- burger-queen/src/Components/Modals/ModalConfirm.js
+++ burger-queen/src/Components/Modals/ModalConfirm.js
@@ -1,23 +1,16 @@
-import { useState } from "react";
 import "./Modal.css";
 
+export const ModalConfirm = ({ setModalOpen }) => {
+  const hiddenModal = () => {
+    setModalOpen(false);
+  };
 
-export const ModalConfirm = () => {
-
-    let [closeModal, setCloseModal] = useState(false)
-    
-    const hiddenModal = () => {
-        setCloseModal(true)
-  }
-
-
-return (
+  return (
     <div className="modal-background">
-        <div className="container-modal">
+      <div className="container-modal">
         <h2>Pedido de xxx confirmado</h2>
         <button onClick={hiddenModal}>LISTO!</button>
-        </div>
-
+      </div>
     </div>
-)
-}
+  );
+};
