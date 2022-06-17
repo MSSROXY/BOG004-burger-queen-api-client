d978cce02c8de8685c1081697626c923bf920ca7
mssroxy <roxysolanopino@gmail.com>
2022-06-17 14:00:31 -0400

    modal edit products ok

--- burger-queen/src/Components/Admin/Admin.js
+++ burger-queen/src/Components/Admin/Admin.js
@@ -2,43 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import backimg from "../../img/backimg.png";
 import addUser from "../../img/addUser.png";
-import { getUsersRequest, getToken, getUser } from "../API/fetch";
+import { getUsersRequest, getToken, productsRequest } from "../API/fetch";
 import { UserCard } from "./UserCard";
 import { ModalRegister } from "../Modals/ModalRegister";
 import "./Admin.css";
-import { ProductCard } from "./ProductCard"
+import { ProductCard } from "./ProductCard";
 
 export const Admin = () => {
-  let [changeView, setChangeView] = useState(true)
+  let [changeView, setChangeView] = useState(true);
   let [users, setUsers] = useState([]);
-  let [showModal , setShowModal] = useState(false);
-  let url = "http://localhost:8080/users";
-  let myToken = getToken();
+  let [products, setProducts] = useState([])
+  let [showModal, setShowModal] = useState(false);
 
   const navigate = useNavigate();
   const back = () => {
     navigate("/select");
   };
 
-const viewUsers = () => {
-  setChangeView(true)
-}
+  const viewUsers = () => {
+    setChangeView(true);
+  };
 
-const viewProducts = () => {
-  setChangeView(false)
-}
-
+  const viewProducts = () => {
+    setChangeView(false);
+  };
 
   useEffect(() => {
     getUsers();
+    getProducts()
   }, []);
 
-  const getUsers = () =>
+  const getProducts =() => {
+    const url = "http://localhost:8080/products";
+    const myToken = getToken();
+    productsRequest(url,myToken).then(res => setProducts(res))
+  }
+  const getUsers = () => {
+    const url = "http://localhost:8080/users";
+    const myToken = getToken();
     getUsersRequest(url, myToken).then((res) => setUsers(res));
+  };
 
   const openModalRegister = () => {
     setShowModal(true);
-  }
+  };
 
   return (
     <div className="div-general">
@@ -57,16 +64,22 @@ const viewProducts = () => {
         <div className="div-orders">
           <div className="div-header">
             <h2>Bienvenido Admin</h2>
-            <button onClick={openModalRegister}><img src={addUser} alt="addUser"></img></button>
+            <button onClick={openModalRegister}>
+              <img src={addUser} alt="addUser"></img>
+            </button>
           </div>
           <div className="div-container-cards">
-            {changeView ? 
-            (users.map((user) => (
-              <UserCard user={user} getUsers={getUsers} />
-            ))) : <ProductCard/> }
-          
-          </div> 
-        {showModal ? <ModalRegister setShowModal={setShowModal} getUsers={getUsers}/> : false}
+            {changeView ? (
+              users.map((user) => <UserCard user={user} getUsers={getUsers} />)
+            ) : (
+              products.map(product => <ProductCard product={product} getProducts={getProducts}/>)
+            )}
+          </div>
+          {showModal ? (
+            <ModalRegister setShowModal={setShowModal} getUsers={getUsers} />
+          ) : (
+            false
+          )}
         </div>
       </div>
     </div>
--- burger-queen/src/Components/Modals/ModalAdmin.js
+++ burger-queen/src/Components/Modals/ModalAdmin.js
@@ -1,23 +1,27 @@
 import { getToken, deleteRequest } from "../API/fetch";
-export const ModalAdmin = ({setShowModal, user, getUsers}) => {
-    const hiddenModal = () => {
-        setShowModal(false)
-    };
-    const deleteUser = () => {
-        const userId = user.id;
-        const myUrl = "http://localhost:8080/users/" + userId;
-        const myToken = getToken();
-        deleteRequest(myUrl, myToken).then((res) => console.log(res));
-        getUsers()
-        setShowModal(false)
-    }
+export const ModalAdmin = ({ setShowModal, user, getUsers }) => {
+  const hiddenModal = () => {
+    setShowModal(false);
+  };
+  const deleteUser = () => {
+    const userId = user.id;
+    const myUrl = "http://localhost:8080/users/" + userId;
+    const myToken = getToken();
+    deleteRequest(myUrl, myToken).then((res) => console.log(res));
+    getUsers();
+    setShowModal(false);
+  };
   return (
     <div className="modal-background">
       <div className="container-modal">
         <h4>¿Está seguro que quiere eliminar el usuario?</h4>
         <div className="container-buttons">
-          <button className="btn-red" onClick={hiddenModal}>NO</button>
-          <button className="btn-green" onClick={deleteUser}>SI</button>
+          <button className="btn-red" onClick={hiddenModal}>
+            NO
+          </button>
+          <button className="btn-green" onClick={deleteUser}>
+            SI
+          </button>
         </div>
       </div>
     </div>
